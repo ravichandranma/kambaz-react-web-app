2bf1dcc8417f1797645d2af76ce043d4d75115f2
ravichandranma <ravichandran.ma@northeastern.edu>
2025-02-21 17:37:51 -0500

    last one

--- src/kambaz/Navigation.tsx
+++ src/kambaz/Navigation.tsx
@@ -28,7 +28,7 @@ export default function KambazNavigation() {
         action
         className="bg-black border-0 text-center"
       >
-        <img src="/images/NEU.png" width="75px" />
+        <img src="/images/NEU.jpg" width="75px" />
       </ListGroup.Item>
       <ListGroup.Item
         as={Link}
