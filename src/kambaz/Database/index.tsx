8f54e9056ff668cae1bcd8390f7501fda294fd48
ravichandranma <ravichandran.ma@northeastern.edu>
2025-04-06 16:18:31 -0400

    change3

--- src/kambaz/Database/index.tsx
+++ src/kambaz/Database/index.tsx
@@ -3,4 +3,5 @@ import modules from "./modules.json";
 import assignments from "./assignments.json";
 import users from "./users.json";
 import enrollments from "./enrollments.json";
+
 export default { courses, assignments, modules, users, enrollments };
